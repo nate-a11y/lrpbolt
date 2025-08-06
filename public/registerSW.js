f49b350362ee09d64c61db4d270b77c7b098d6c9
Nate <nate@lakeridepros.com>
2025-08-06 09:28:09 -0500

    Update registerSW.js

--- public/registerSW.js
+++ public/registerSW.js
@@ -2,6 +2,16 @@ import { registerSW } from 'virtual:pwa-register';
 
 registerSW({
   immediate: true,
-  onNeedRefresh() {},
-  onOfflineReady() {},
+  onNeedRefresh() {
+    console.log('[PWA] New content available, refresh needed.');
+  },
+  onOfflineReady() {
+    console.log('[PWA] App ready to work offline.');
+  },
+  onRegistered(registration) {
+    console.log('[PWA] Service worker registered:', registration);
+  },
+  onRegisterError(error) {
+    console.error('[PWA] Service worker registration failed:', error);
+  },
 });
