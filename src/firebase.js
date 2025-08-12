76cbf46a66e5245629478c1f1ae8b23b41e26251
Nate <nate@lakeridepros.com>
2025-08-11 20:07:19 -0500

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,18 @@
+// src/firebase.js
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore, getFirestore } from "firebase/firestore";
 
-// TODO: move secrets to server; client envs remain for now.
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  // add others if you already had them (storageBucket, messagingSenderId, appId)
 };
 
 const app = getApps().length ? getApp() : initializeApp(firebaseConfig);
+
+// Set Firestore settings BEFORE first getFirestore()
+initializeFirestore(app, { ignoreUndefinedProperties: true });
 
 export const auth = getAuth(app);
 export const db = getFirestore(app);
