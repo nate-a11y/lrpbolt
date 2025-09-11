998631ed400e1a77008437e1c6eff7cc195276e5
Nate <nate@lakeridepros.com>
2025-09-11 02:09:35 -0500

    chore: tighten vehicle overview layout

--- src/pages/ClaimRides.jsx
+++ src/pages/ClaimRides.jsx
@@ -141,10 +141,7 @@ export default function ClaimRides() {
 
   if (!liveRides.length)
     return (
-      <EmptyRideState
-        refreshIn={refreshIntervalSec}
-        onRefresh={refetch}
-      />
+      <EmptyRideState refreshIn={refreshIntervalSec} onRefresh={refetch} />
     );
 
   return (
