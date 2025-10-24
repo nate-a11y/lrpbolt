fb7ae9c1f6bac71da87205c58eb3fed508899742
Nate <nate@lakeridepros.com>
2025-10-24 16:35:49 -0500

    Refine Vite chunking for MUI grid overrides

--- src/muiOverrides/gridFocusedVirtualCellSelector.js
+++ src/muiOverrides/gridFocusedVirtualCellSelector.js
@@ -1,6 +1,8 @@
 import _extends from "@babel/runtime/helpers/esm/extends";
-import { createSelector } from "reselect";
-import { createSelectorMemoized } from "@mui/x-data-grid/utils/createSelector.js";
+import {
+  createSelector,
+  createSelectorMemoized,
+} from "@mui/x-data-grid/utils/createSelector.js";
 import { gridVisibleColumnDefinitionsSelector } from "@mui/x-data-grid/hooks/features/columns/gridColumnsSelector.js";
 import { gridRenderContextSelector } from "@mui/x-data-grid/hooks/features/virtualization/gridVirtualizationSelectors.js";
 import { gridFocusCellSelector } from "@mui/x-data-grid/hooks/features/focus/index.js";
